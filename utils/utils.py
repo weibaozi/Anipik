e6e9954f585f57e06ed5c6814efafb59b7126492
weibaozi <guo361270936@gmail.com>
2023-12-10 19:27:43 -0500

    fix utils

--- utils/utils.py
+++ utils/utils.py
@@ -186,9 +186,7 @@ def safe_dump_yaml(data,file):
             time.sleep(0.1)
             continue
 def rename_folder(old,new,location=''):
-    old='test'
     old=os.path.join(location,old)
-    new='test2'
     new=os.path.join(location,new)
     try:
         if os.path.exists(new):
