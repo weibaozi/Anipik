df2e72243cf29725794561fe4008771b3547c284
weibaozi <guo361270936@gmail.com>
2023-12-16 14:39:02 -0500

    fix notify

--- chatbot/notify.py
+++ chatbot/notify.py
@@ -5,11 +5,13 @@ NOTIFY_QUEUE_DIR = os.path.join(CURRENT_DIR, "notify_queue.yaml")
 def notify_discord(message,setting,user_id=None,channel_id=None):
     """notify discord user or channel"""
     with open(NOTIFY_QUEUE_DIR, "r+",encoding='utf-8') as f:
-        print(NOTIFY_QUEUE_DIR)
+        # print(NOTIFY_QUEUE_DIR)
         notify_queue=yaml.load(f, Loader=yaml.FullLoader)
+        # print(notify_queue)
         if notify_queue is None:
-            print("notify_queue.yaml not found")
-            return 
+            notify_queue={'discord_user':{},'discord_channel':{}}
+            # print("notify_queue.yaml not found")
+            # return 
         # print(notify_queue)
         if user_id==None and channel_id==None:
             for id in setting['discord_id']['user_id']:
@@ -18,6 +20,9 @@ def notify_discord(message,setting,user_id=None,channel_id=None):
                 notify_queue['discord_user'][id].append(message)
         elif user_id != None: # only notify if user_id is in the list
             if user_id in setting['discord_id']['user_id']:
+                #check if user_id is in the list
+                if user_id not in notify_queue['discord_user']:
+                    notify_queue['discord_user'][user_id]=[]
                 notify_queue['discord_user'][user_id].append(message)
             else:
                 print(f"discord user {user_id} not in the list")
@@ -29,6 +34,8 @@ def notify_discord(message,setting,user_id=None,channel_id=None):
                 notify_queue['discord_channel'][id].append(message)
         elif channel_id != None:
             if channel_id in setting['discord_id']['channel_id']:
+                if channel_id not in notify_queue['discord_channel']:
+                    notify_queue['discord_channel'][channel_id]=[]
                 notify_queue['discord_channel'][channel_id].append(message)
             else:
                 print(f"discord channel {channel_id} not in the list")
